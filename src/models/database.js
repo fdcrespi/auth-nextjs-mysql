1eed05bd8b3a5d90c91ad6fe79eb50fe3a8c18ab
Federico Crespi <crespif@celta.com.ar>
2023-03-29 10:53:46 -0300

    Se agregaron las variables a .env

--- src/models/database.js
+++ src/models/database.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config({path: '.env'});
+
+console.log(process.env.DB_HOST);
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'celtatsas_sucvirtual'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((error) => {
